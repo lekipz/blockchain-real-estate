f00508d021af3cea6af49b419a7d2554d7202e94
unknown <mbetourne@norsys.fr>
2021-02-24 17:58:21 +0100

    feat(): more css on preview

--- client/src/productList/RealEstateListPage.js
+++ client/src/productList/RealEstateListPage.js
@@ -7,7 +7,7 @@ export default function RealEstateListPage() {
       {
         tokenId: 1,
         name: 'Le uc de Raumé',
-        description: 'Un snack',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         price: 3.14,
         address: 'fjdshfudshfsdjnfèdshfjii',
         images: ['https://assets.change.org/photos/4/ji/md/eIjImDqzbMmvALO-800x450-noPad.jpg?1539879623'],
--- client/src/productList/RealEstatePreview.js
+++ client/src/productList/RealEstatePreview.js
@@ -1,13 +1,14 @@
 import classes from './cssModules/RealEstatePreview.module.css'
 
 export default function RealEstatePreview({ realEstate }) {
+
   return (
     <div className={classes.realEstatePreview}>
-      <p>Nom: {realEstate.name}</p>
-      <p>Description: {realEstate.description}</p>
-      <p>Prix: {realEstate.price}</p>
-      <p>Adresse: {realEstate.address}</p>
-      <img className="object-fill h-40" src={realEstate.images[0]}/>
+      <p><span className="font-bold">Nom: </span> {realEstate.name}</p>
+      <p><span className="font-bold">Description:</span> {realEstate.description.slice(0,150)+'...'}</p>
+      <p><span className="font-bold">Prix:</span> {realEstate.price}</p>
+      <p><span className="font-bold">Adresse:</span> {realEstate.address}</p>
+      <img className="object-contain h-40 pt-3" src={realEstate.images[0]}/>
     </div>
   )
 }
