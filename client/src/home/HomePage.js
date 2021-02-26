cc1f9b39a49d65759032a9f9be666316502330af
Corentin Delannoy <corentin@MacBook-Pro-de-Corentin.local>
2021-02-26 14:04:27 +0100

    fix(contract): rename contract in whole app

--- client/src/home/HomePage.js
+++ client/src/home/HomePage.js
@@ -1,4 +1,9 @@
+import { useRealEstates } from '../real-estates';
+
 export default function HomePage() {
+  const realEstates = useRealEstates();
+  console.log(realEstates);
+
   return (
     <div>
       This is the homepage.
