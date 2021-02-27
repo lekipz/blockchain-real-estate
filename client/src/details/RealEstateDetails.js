0559d09444362d640ed252d1e5df223b2c02c76b
corentind59 <corentin.delannoy@supinfo.com>
2021-02-27 16:21:43 +0100

    feat(buy real estate): add button on details page to buy token

--- client/src/details/RealEstateDetails.js
+++ client/src/details/RealEstateDetails.js
@@ -1,10 +1,11 @@
 import classes from './RealEstateDetails.module.css';
 import DemoCarousel from '../common/ui/Carousel';
 import { formatEth } from '../common/utils';
+import Button from '../common/ui/Button';
 
-export default function RealEstateDetails({ realEstate }) {
+export default function RealEstateDetails({ realEstate, onBuy, loading }) {
   return (
-    <div className="flex flex-nowrap justify-around">
+    <div className="flex flex-nowrap justify-between">
       <div className={classes.informations}>
         <h2 className="text-3xl mb-2 text-center">{realEstate.name}</h2>
         <section className="md:max-w-6xl mx-auto pt-4 px-4">
@@ -22,6 +23,13 @@ export default function RealEstateDetails({ realEstate }) {
               {realEstate.description}
             </p>
           </section>
+        )}
+        {!realEstate.isOwned && realEstate.onSale && (
+          <div className="flex flex-row-reverse pt-8">
+            <Button type="button" onClick={onBuy} disabled={loading}>
+              {loading ? 'Achat en cours...' : 'Acheter'}
+            </Button>
+          </div>
         )}
       </div>
       <DemoCarousel pictures={realEstate.images}/>
--- client/src/details/RealEstateDetailsPage.js
+++ client/src/details/RealEstateDetailsPage.js
@@ -1,19 +1,39 @@
-import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import { useRealEstate } from '../real-estates';
 import ScreenSpinner from '../common/ui/ScreenSpinner';
 import RealEstateDetails from './RealEstateDetails';
+import { useCacheSend } from '../drizzle/utils/useCacheSend';
+import { useDrizzle } from '../drizzle';
 
 export default function RealEstateDetailsPage() {
   const { id } = useParams();
+  const history = useHistory();
   const realEstate = useRealEstate(+id);
+  const { drizzle, drizzleState } = useDrizzle();
+  const { sendTransaction: buy, status } = useCacheSend('buy');
+  useEffect(() => {
+    if (status === 'success') {
+      history.push('/');
+    }
+  }, [status]);
 
   if (!realEstate) {
     return <ScreenSpinner label="Chargement du bien..."/>;
   }
 
+  const handleBuy = () => {
+    buy(realEstate.tokenId, {
+      from: drizzleState.accounts[0],
+      value: drizzle.web3.utils.toWei(realEstate.price.toString())
+    });
+  };
+
   return (
     <div className="px-4">
-      <RealEstateDetails realEstate={realEstate}/>
+      <RealEstateDetails realEstate={realEstate}
+                         onBuy={handleBuy}
+                         loading={status === 'loading'}/>
     </div>
   );
 }
