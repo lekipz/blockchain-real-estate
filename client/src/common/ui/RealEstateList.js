f7175f768a9f30bcffccf086d7f2128c362f2e0e
unknown <mbetourne@norsys.fr>
2021-02-28 18:35:18 +0100

    fix(): unique key on RealEstateList

--- client/src/common/ui/RealEstateList.js
+++ client/src/common/ui/RealEstateList.js
@@ -1,10 +1,11 @@
 import RealEstatePreview from "./RealEstatePreview";
 
 export default function RealEstateList({reList}) {
+
   return(
     <div className="flex flex-wrap justify-center mt-28 m-72">
       {
-        reList.map(re => (<RealEstatePreview realEstate={re} key={re.tokenId}/>))
+        reList.filter(re => !!re && !!re.tokenId).map(re => (<RealEstatePreview realEstate={re} key={re.tokenId}/>))
       }
     </div>
   )
